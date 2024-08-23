e45d505da66f04c1d65586c6db5f5eb155219752
Alu <afa2409@gmail.com>
2024-08-22 22:28:11 -0300

    trabajado el algoritmo de batalla, cada ataque tiene hardcodeado una chance de evade en base al nuevo atributo de personajes, Agilidad

--- batalla.py
+++ batalla.py
@@ -3,7 +3,7 @@ import json
 import suerte
 
 
-
+#el json trae a los enemigos desde la 'base de datos'
 with open('actors.json', 'r') as file:
     data = json.load(file)
 
@@ -16,6 +16,7 @@ heroe = {
         'nombre': 'Aiken',
         "hp": 270,
         "atk": 10,
+        "agi": 20,
         "def": 5,
         "lk": suerte.suerte,
         "arma": 10,
@@ -26,19 +27,29 @@ enemigo = {
     "nombre": contrincante["nombre"],
     "hp": contrincante["hp"],
     "atk": contrincante["atk"],
+    "agi": contrincante["agi"],
     "def": contrincante["def"]
 }
 
+#esta función la voy a diseñar para usar después de cada ataque, para dar una tirada de suerte con chance de evitar el daño
+def ac_evade(actor):
+    dado = suerte.tirada(actor.get('agi'))
+    return dado
 
 def ac_ataque(actor, vs):
     if 'arma' in actor:
         golpe = actor['atk'] * ((actor['arma']/10) + 1)
     else:
         golpe = actor['atk']
-    if suerte.tirada():
-        vs['hp'] -= golpe
-        print(f"El ataque de {actor.get('nombre')} conectó por un total de {golpe}")
+    print(f'¡{actor} lanza un ataque por {golpe} de daño!')
+    if ac_evade(vs):
+        print(f"{vs.get('nombre')} esquivó el ataque")
     else:
-        print(f"{actor.get('nombre', 'El actor')} falló el ataque.")
+        if suerte.tirada(suerte.suerte+50):
+            vs['hp'] -= golpe
+            print(f"El ataque de {actor.get('nombre')} conectó por un total de {golpe}")
+        else:
+            print(f"{actor.get('nombre')} falló el ataque.")
 
-ac_ataque(enemigo, heroe)
+
+
--- suerte.py
+++ suerte.py
@@ -2,7 +2,7 @@ import random as r
 import json
 
 #el stat de suerte puede ser ligado a un stat dentro de un diccionario que represente las estadísticas del actor en el juego
-suerte = 90
+suerte = 30
 impares = (1, 3, 5, 7, 9)
 pares = (2, 4, 6, 8)
 #función lambda para tomar un elemento random de una lista, esto es una función de random pero no tenemos permitido usarla
@@ -17,7 +17,7 @@ def shuffle(lista):
     return lista
 
 #genero la ruleta
-def gensuerte():
+def gensuerte(suerte):
     ruleta = []
     restantes = 100 - suerte
     for _ in range(suerte): #genero números pares
@@ -29,12 +29,10 @@ def gensuerte():
     shuffle(ruleta)
     return ruleta
 
-def tirada():
-    ruleta = gensuerte()
+def tirada(suerte):
+    ruleta = gensuerte(suerte)
     output = choice(ruleta)
     if output % 2 == 0:
         return True
     else:
         return False
-    
-print(tirada())
