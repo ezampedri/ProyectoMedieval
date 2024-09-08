1bcd410f068cb003d37098fba83e33c07bb88264
Alu <afa2409@gmail.com>
2024-09-07 21:55:30 -0300

    Listo el árbol de decisiones del heroe, ya debería estar todo listo para empezar a trabajar en el flujo de batalla principal

--- battle/battle.py
+++ battle/battle.py
@@ -37,7 +37,7 @@ enemigo = {
 
 #esta función la voy a diseñar para usar después de cada ataque, para dar una tirada de suerte con chance de evitar el daño
 def ac_evade(actor):
-    dado = suerte.tirada(actor.get('agi'))
+    dado = suerte.tirada(actor['agi'])
     return dado
 
 
@@ -58,7 +58,7 @@ def ac_ataque(actor, vs):
 
 #para la magia va a ser un poco más complicado, se tienen que pasar el argumento del heroe, el vs, y la magia en cuestión
 def ac_magia(actor, vs, habilidad):
-    with open('ablt.json', 'r') as file:
+    with open('habilidades.json', 'r') as file:
         data = json.load(file)['habilidades']
     for m in data:
         if m['nombre'] == habilidad:
@@ -76,3 +76,40 @@ def interfaz(heroe, enemigo):
     print(f"MP: {heroe['mp']:<17} | MP: {enemigo.get('mp', 'N/A'):<20}")
     print("===================================")
 
+#esta función termina la batalla sin recompensa
+def escape():
+    escapada = suerte.tirada((suerte.suerte//2)+heroe['agi'])
+    return escapada
+
+
+def acc(heroe, enemigo):
+    bandera= True
+    while bandera:
+        interfaz(heroe, enemigo)
+        print(f"\n \n Escribe tu acción!")
+        print(f"Atacar")
+        print(f"Habilidades")
+        print(f"Evadir")
+        print(f"Escapar")
+        user= input("¡Tomá el destino en tus manos! ")
+        if user.lower == "atacar":
+            ac_ataque(heroe, enemigo)
+        elif user.lower == "habilidades":
+            print(f"Elige una habilidad!")
+            for habilidad in heroe['habilidades']:
+                print(habilidad)
+            user = input()
+            while True:
+                for habilidad in heroe['habilidades']:
+                    if user == habilidad:
+                        ac_magia(habilidad)
+                        break #acá uso break para que Juli me diga si este es un buen uso del break o no
+                    else:
+                        print(f'Ese conjuro no forma parte del conocimiento de {heroe}')
+        elif user.lower == "evadir":
+            print(f'{heroe} ¡se prepara para evadir el próximo ataque!')    
+            heroe['agi'] * 2 #recordar dividir este parámetro después de que pase el turno en el módulo principal de batalla
+        elif user.lower == 'escapar':
+            escape()
+        else:
+            print('No se reconoce el comando!')
