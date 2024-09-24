30ea73f80cb7440cca14f4f54692f4a68171ec4c
Macarena Lara Molina <molina.macarenalara@gmail.com>
2024-09-24 00:58:50 -0300

    se corrigen las funciones de menu, se crean funciones de rejuvenecer y curarse

--- assets/champions.py
+++ assets/champions.py
@@ -89,3 +89,51 @@ champions = {
         }
     ]
 }
+
+
+def curarse(nombre_heroe):
+    for heroe in champions["heroe"]:
+        if heroe["nombre"] == nombre_heroe:
+            heroe["hp"] +=50
+
+def rejuvenecer(nombre_heroe):
+    for heroe in champions["heroe"]:
+        if heroe["nombre"] == nombre_heroe:
+            if nombre_heroe == "Gandalf":
+                heroe["hp"] -= 5
+
+def mostrar_heroes():
+    print("Héroes:")
+    for heroe in champions["heroe"]:
+        print(f"Nombre: {heroe['nombre']}, Clase: {heroe['clase']}, HP: {heroe['hp']}, MP: {heroe['mp']}, ATK: {heroe['atk']}, MAG: {heroe['mag']}, AGI: {heroe['agi']}, DEF: {heroe['def']}, LK: {heroe['lk']}, EXP: {heroe['exp']}, Habilidades: {', '.join(heroe['habilidades'])}")
+
+def mostrar_enemigos():
+    print("Enemigos:")
+    for enemigo in champions["enemigos"]:
+        print(f"Nombre: {enemigo['nombre']}, HP: {enemigo['hp']}, MP: {enemigo['mp']}, ATK: {enemigo['atk']}, MAG: {enemigo['mag']}, AGI: {enemigo['agi']}, DEF: {enemigo['def']}, LK: {enemigo['lk']}, EXP: {enemigo['exp']}")
+
+def menu():
+    while True:
+        print('---------------------')
+        print('- Proyecto Medieval -')
+        print('---------------------')
+        print("\nMenu:")
+        print("1. Ver Héroes")
+        print("2. Ver Enemigos")
+        print("3. Salir")
+        print('---------------------')
+        opcion = input("Elige una opción: ")
+
+        if opcion == '1':
+            mostrar_heroes()
+        elif opcion == '2':
+            mostrar_enemigos()
+        elif opcion == '3':
+            print("Saliendo del programa...")
+            break
+        else:
+            print("Opción no válida. Inténtalo de nuevo.")
+
+
+menu()
+                
