a775bbc9d93373c062531583b1d974ba30a5ab70
Macarena Lara Molina <molina.macarenalara@gmail.com>
2024-10-13 03:14:55 -0300

    se actualiza con try except el cargar partida y se agrega variable de current save

--- battle/partida.py
+++ battle/partida.py
@@ -1,22 +1,33 @@
-import json
+import json, time
 
-partida=[]
+partida_data=[]
 
-historial_partidas = []
+historial_partidas_data = []
 
-def guardar_partida(partida, nombre_archivo="partida.json"):
-    with open(nombre_archivo, 'w') as archivo:
+def guardar_partida(partida):
+    with open("partida.json", 'w') as archivo:
         json.dump(partida, archivo, indent=4)
 
 def cargar_partida(nombre_archivo="partida.json"):
-    with open(nombre_archivo, 'r') as archivo:
-        return json.load(archivo)
+    try:
+        
+        with open(nombre_archivo, 'r') as archivo:
+            return json.load(archivo)
+    except FileNotFoundError:
+        return partida_data
     
-def actualizar_y_guardar(partida, historial, nombre_archivo="partida.json"):
+def actualizar_y_guardar(partida, historial):
     historial.append(partida.copy())  
-    with open(nombre_archivo, 'w') as archivo:
+    with open("partida.json", 'w') as archivo:
         json.dump(historial, archivo, indent=4)
         
-def cargar_historial(nombre_archivo="partida.json"):
-    with open(nombre_archivo, 'r') as archivo:
+def cargar_historial():
+    with open("partida.json", 'r') as archivo:
         return json.load(archivo)
+    
+
+current_save = cargar_partida()
+
+##in progress
+##while True:
+   ## guardar_partida()
