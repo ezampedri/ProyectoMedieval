13b6706b40a41890c6a07be1d38955b6da7b8223
Macarena Lara Molina <molina.macarenalara@gmail.com>
2024-10-13 02:44:33 -0300

    se actualizan los open de los json

--- battle/partida.py
+++ battle/partida.py
@@ -5,18 +5,18 @@ partida=[]
 historial_partidas = []
 
 def guardar_partida(partida, nombre_archivo="partida.json"):
-    with open(nombre_archivo, 'guardar') as archivo:
+    with open(nombre_archivo, 'w') as archivo:
         json.dump(partida, archivo, indent=4)
 
 def cargar_partida(nombre_archivo="partida.json"):
-    with open(nombre_archivo, 'leer') as archivo:
+    with open(nombre_archivo, 'r') as archivo:
         return json.load(archivo)
     
 def actualizar_y_guardar(partida, historial, nombre_archivo="partida.json"):
     historial.append(partida.copy())  
-    with open(nombre_archivo, 'actualizar') as archivo:
+    with open(nombre_archivo, 'w') as archivo:
         json.dump(historial, archivo, indent=4)
         
 def cargar_historial(nombre_archivo="partida.json"):
-    with open(nombre_archivo, 'cargarHistorial') as archivo:
+    with open(nombre_archivo, 'r') as archivo:
         return json.load(archivo)
